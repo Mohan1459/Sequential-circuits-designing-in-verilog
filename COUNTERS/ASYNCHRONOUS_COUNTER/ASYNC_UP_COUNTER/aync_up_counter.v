133db2180074aa101b4ded2f90c328e6ef565b9b
Mohan1459 <157452661+Mohan1459@users.noreply.github.com>
2025-09-19 19:00:35 +0530

    Update aync_up_counter.v

--- COUNTERS/ASYNCHRONOUS_COUNTER/ASYNC_UP_COUNTER/aync_up_counter.v
+++ COUNTERS/ASYNCHRONOUS_COUNTER/ASYNC_UP_COUNTER/aync_up_counter.v
@@ -5,10 +5,10 @@ module counter(
 );
   always@(posedge clk or negedge rst)begin
     if(!rst)begin
-      count<=4'b1111;
+      count<=4'b0000;
     end
     else begin
-      count<=count-1;
+      count<=count+1;
     end
   end
 endmodule
