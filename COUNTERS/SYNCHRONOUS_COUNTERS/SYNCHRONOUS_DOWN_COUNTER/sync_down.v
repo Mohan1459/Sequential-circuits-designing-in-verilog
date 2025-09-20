e54f393d1823d3262eed4f1489c13f2d630bd88b
Mohan1459 <157452661+Mohan1459@users.noreply.github.com>
2025-09-20 10:33:48 +0530

    Create sync_down.v
    the synchronous down counter is implemented in behavioral modeling in verilog

---
+++ COUNTERS/SYNCHRONOUS_COUNTERS/SYNCHRONOUS_DOWN_COUNTER/sync_down.v
@@ -0,0 +1,15 @@
+module counter(
+  input clk,
+  input rst,
+  output reg [3:0]count
+);
+  always@(posedge clk)begin
+    if(rst)begin
+      count<=4'b1111;
+    end
+    else begin
+      count<=count-1;
+    end
+  end
+endmodule
+
