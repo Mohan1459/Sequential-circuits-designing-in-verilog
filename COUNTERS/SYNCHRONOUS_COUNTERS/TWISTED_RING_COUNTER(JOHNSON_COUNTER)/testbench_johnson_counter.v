7db78fc080aef9a39948381e8a2f03d31d39a1c9
Mohan1459 <157452661+Mohan1459@users.noreply.github.com>
2025-09-21 12:22:23 +0530

    Create testbench_johnson_counter.v

---
+++ COUNTERS/SYNCHRONOUS_COUNTERS/TWISTED_RING_COUNTER(JOHNSON_COUNTER)/testbench_johnson_counter.v
@@ -0,0 +1,22 @@
+module tb;
+  reg clk;
+  reg rst;
+  wire [3:0]q;
+  johnson_counter g1(.clk(clk),.rst(rst),.q(q));
+  initial clk=0;
+  always #5clk=~clk;
+  initial begin
+    rst=1'b1;
+    #10;
+    rst=1'b0;
+    #80;
+    $finish;
+  end
+  initial begin
+    $monitor("Time=%d,clk=%b,reset=%b,output=%b",$time,clk,rst,q);
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+endmodule
